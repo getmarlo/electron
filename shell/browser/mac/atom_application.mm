ee9babe600204bf6e31866381c356ad68c184ab4
Nitin Rajinikanth <nitin.rajinikanth@gmail.com>
2020-04-11 11:13:38 -0500

    adding zoom hotkey local event monitor workaround to forked v7 of electron

--- shell/browser/mac/atom_application.mm
+++ shell/browser/mac/atom_application.mm
@@ -61,9 +61,19 @@ inline void dispatch_sync_main(dispatch_block_t block) {
 
 - (void)sendEvent:(NSEvent*)event {
   base::AutoReset<BOOL> scoper(&handlingSendEvent_, YES);
-  content::ScopedNotifyNativeEventProcessorObserver scopedObserverNotifier(
+  if (event.type == NSEventTypeKeyDown && event.keyCode == 49) {
+    // Skipping space bar handling
+    NSWindow* nextWindow = event.window;
+    [nextWindow sendEvent:event];
+  } else if (event.type == NSEventTypeKeyDown && event.keyCode == 19 && event.modifierFlags & NSEventModifierFlagCommand) {
+    // Skipping CMD+2 handling
+    NSWindow* nextWindow = event.window;
+    [nextWindow sendEvent:event];
+  } else {
+    content::ScopedNotifyNativeEventProcessorObserver scopedObserverNotifier(
       &observers_, event);
-  [super sendEvent:event];
+    [super sendEvent:event];
+  }
 }
 
 - (void)setHandlingSendEvent:(BOOL)handlingSendEvent {
